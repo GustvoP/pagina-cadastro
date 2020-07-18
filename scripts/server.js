bf0f7ec88013ddb205b3d886c91dd8da197b7213
Gustavo Poffo <gustavoxpoffo@gmail.com>
2020-07-17 23:57:46 -0300

    alterando path e criando servidor

---
+++ scripts/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/usuarios", (req, resp) => {
+  console.log(req.body);
+  resp.send("<h1>Parabéns. Usuário Incluido!</h1>");
+});
+
+app.listen(3003);
